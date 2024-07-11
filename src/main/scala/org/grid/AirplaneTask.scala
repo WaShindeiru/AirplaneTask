baeb87a4c96b51c06ea3d9ba30b00737c1d4342a
WaShindeiru <franekdata@gmail.com>
2024-07-11 17:50:04 +0200

    finish airplane task

---
+++ src/main/scala/org/grid/AirplaneTask.scala
@@ -0,0 +1,35 @@
+package org.grid
+
+import scala.util.Try
+
+object AirplaneTask {
+
+  def solution(numRows: Int, reservations: String): Int = {
+    val charMap = Try {
+      reservations
+        .split(" ").foldLeft(Map[Int, List[Char]]()) {
+          (acc: Map[Int, List[Char]], i: String) => {
+            val key = i.charAt(0).asDigit
+            acc.updated(key, acc.getOrElse(i.charAt(0).asDigit, List.empty[Char]) :+ i.charAt(1))
+          }
+        }
+    }.getOrElse(Map[Int, List[Char]]())
+
+    println(charMap)
+
+    (1 to numRows)
+      .map { (i: Int) =>
+        List("ABC", "DEF", "EFG", "HJK")
+          .filterNot { (s: String) => charMap.getOrElse(i, List[Char]()).exists(s.contains) }
+      }
+      .map { (lst: List[String]) => lst match {
+        case List(_*) if lst.contains("DEF") && lst.contains("EFG") => lst.filterNot(_ == "DEF")
+        case List(_*) => lst
+      }}
+      .map { (l: List[String]) => l.length }.sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(solution(1, ""))
+  }
+}
